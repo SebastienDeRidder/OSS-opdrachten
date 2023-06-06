525e2184377909eac2766d100afe3216b615d5e9
SebastienDeRidder <sebastien.deridder13@gmail.com>
2023-06-06 08:38:43 -0700

    Problemen met -Path na bloed, zweet en tranen opgelost

--- Powershell/azureNaarLokaleAD.ps1
+++ Powershell/azureNaarLokaleAD.ps1
@@ -8,17 +8,17 @@ $ouParentExists = Get-ADOrganizationalUnit -Filter "Name -eq '$ouParentName'"
 
 # Maak de OU's aan als deze niet bestaan
 if (-not $ouParentExists) {
-    New-ADOrganizationalUnit -Name $ouParentName -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -Name $ouParentName -Path "DC=sebas-tien,DC=com" -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouParentName' succesvol aangemaakt."
 
     # Maak de sub-OU aan
-    New-ADOrganizationalUnit -Name 's122861' -Path "OU=$ouParentName,DC=sebas-tien.com,DC=local"
+    New-ADOrganizationalUnit -Name 's122861' -Path "OU=OS Scripting 23,DC=sebas-tien,DC=com" -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouChildName' succesvol aangemaakt binnen OU '$ouParentName'."
 
     # Maak de sub-OU's "groups" en "users" binnen de sub-OU aan
-    New-ADOrganizationalUnit -Name $ouGroupsName -Path "OU=$ouChildName,OU=$ouParentName,DC=sebas-tien.com,DC=local" -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -Name $ouGroupsName -Path "OU=s122861,OU=OS Scripting 23,DC=sebas-tien,DC=com" -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouGroupsName' succesvol aangemaakt binnen OU '$ouChildName'."
-    New-ADOrganizationalUnit -Name $ouUsersName -Path "OU=$ouChildName,OU=$ouParentName,DC=sebas-tien.com,DC=local" -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit -Name $ouUsersName -Path "OU=s122861,OU=OS Scripting 23,DC=sebas-tien,DC=com" -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouUsersName' succesvol aangemaakt binnen OU '$ouChildName'."
 } else {
     Write-Host "OU '$ouParentName' bestaat al. Overslaan."
