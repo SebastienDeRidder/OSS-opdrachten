82f08c092dfaae6046c277bd095f8168fbdb39c7
SebastienDeRidder <sebastien.deridder13@gmail.com>
2023-06-06 08:03:39 -0700

    niet werkende code

--- Powershell/azureNaarLokaleAD.ps1
+++ Powershell/azureNaarLokaleAD.ps1
@@ -1,6 +1,5 @@
 $ouParentName = "OS Scripting 23"  # Naam van de bovenliggende OU
 $ouChildName = "s122861"  # Naam van de sub-OU
-$ouChildPath = "OU=$ouChildName,OU=$ouParentName,DC=sebas-tien.com,DC=local" # Pad in AD
 $ouGroupsName = "groups"
 $ouUsersName = "users"
 
@@ -9,17 +8,17 @@ $ouParentExists = Get-ADOrganizationalUnit -Filter "Name -eq '$ouParentName'"
 
 # Maak de OU's aan als deze niet bestaan
 if (-not $ouParentExists) {
-    New-ADOrganizationalUnit -Name $ouParentName
+    New-ADOrganizationalUnit -Name $ouParentName -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouParentName' succesvol aangemaakt."
 
     # Maak de sub-OU aan
-    New-ADOrganizationalUnit -Name $ouChildName -Path $ouParentPath
+    New-ADOrganizationalUnit -Name 's122861' -Path "OU=$ouParentName,DC=sebas-tien.com,DC=local"
     Write-Host "OU '$ouChildName' succesvol aangemaakt binnen OU '$ouParentName'."
 
     # Maak de sub-OU's "groups" en "users" binnen de sub-OU aan
-    New-ADOrganizationalUnit -Name $ouGroupsName -Path $ouChildPath
+    New-ADOrganizationalUnit -Name $ouGroupsName -Path "OU=$ouChildName,OU=$ouParentName,DC=sebas-tien.com,DC=local" -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouGroupsName' succesvol aangemaakt binnen OU '$ouChildName'."
-    New-ADOrganizationalUnit -Name $ouUsersName -Path $ouChildPath
+    New-ADOrganizationalUnit -Name $ouUsersName -Path "OU=$ouChildName,OU=$ouParentName,DC=sebas-tien.com,DC=local" -ProtectedFromAccidentalDeletion $false
     Write-Host "OU '$ouUsersName' succesvol aangemaakt binnen OU '$ouChildName'."
 } else {
     Write-Host "OU '$ouParentName' bestaat al. Overslaan."
